48a83390dfc71d26c2be67110b48820dd2d49ecd
Goncalo Goncalves <ggoncalves@login004.lca.uc.pt>
2022-03-21 15:46:06 +0000

    Cluster

--- gen_data.sh
+++ gen_data.sh
@@ -1,18 +1,64 @@
 #!/bin/bash
-source /home/goncalo/gw_env/bin/activate
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#                     Slurm Construction Section
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-N=16000
+
+# job name
+#SBATCH --job-name=generate-pycbc
+
+#SBATCH -A mlgw
+
+# number of requested nodes
+#SBATCH --nodes=2
+##SBATCH --ntasks=1
+
+#SBATCH --exclusive
+
+##SBATCH --ntasks-per-node=1
+##SBATCH --cpus-per-task=80
+##SBATCH --mem=64G
+#SBATCH --part=cpu1
+
+# requested runtime
+#SBATCH --time=48:00:00
+
+# Path to the standard output and error files relative to the working directory
+#SBATCH --output=/veracruz/home/g/ggoncalves/gw_classification/jobs/job_generate_%j.log
+
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+#                     User Construction Section
+#::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+ml load OpenBLAS
+ml load Python
+ml load OpenMPI
+ml load libsndfile
+ml load HDF5
+
+HOME_DIR=/veracruz/home/g/ggoncalves/gw_classification
+
+# Install requirements
+python -m venv ${HOME_DIR}/utils/venv
+source ${HOME_DIR}/utils/venv/bin/activate
+
+pip install --upgrade pip==21.3.1
+pip install -r ${HOME_DIR}/requirements.txt
+
+N=160
 POLARIZATION=hp
-MODEL_FILE="/home/goncalo/GW_pycbc/AST/modules/models.csv"
+MODEL_FILE=${HOME_DIR}/utils/models.csv
+
 TYPE_DS="train"
 
-JOB_DIR=/home/goncalo/gw_classification/utils
+JOB_DIR=${HOME_DIR}/utils
 
 SCRIPT1=${JOB_DIR}/generate_mpi.py
-SCRIPT2=${JOB_DIR}/spec_ex.py
-SCRIPT3=${JOB_DIR}/merge_multi.py
 
-OUT_DIR=/home/goncalo/gw_classification/dataset
+# SCRIPT2=${JOB_DIR}/spec_ex.py
+# SCRIPT3=${JOB_DIR}/merge_multi.py
+
+OUT_DIR=/veracruz/projects/m/mlgw/class_dataset
 
 for EOS in BSk20 TM1 SLY9
 
@@ -21,7 +67,7 @@ do
 EOS_DIR=${OUT_DIR}/${TYPE_DS}/${EOS}
 mkdir -p ${EOS_DIR}
 
-mpirun /home/goncalo/gw_env/bin/python ${SCRIPT1} ${N} \
+mpirun python ${SCRIPT1} ${N} \
 -w ${EOS_DIR} \
 -p ${POLARIZATION} \
 --model-name ${EOS} \
@@ -29,5 +75,5 @@ mpirun /home/goncalo/gw_env/bin/python ${SCRIPT1} ${N} \
 --process
 done
 
-/home/goncalo/gw_env/bin/python ${SCRIPT2} -w ${OUT_DIR}/${TYPE_DS}
-/home/goncalo/gw_env/bin/python ${SCRIPT3} -w ${OUT_DIR}/${TYPE_DS}
+# /home/goncalo/gw_env/bin/python ${SCRIPT2} -w ${OUT_DIR}/${TYPE_DS}
+# /home/goncalo/gw_env/bin/python ${SCRIPT3} -w ${OUT_DIR}/${TYPE_DS}
