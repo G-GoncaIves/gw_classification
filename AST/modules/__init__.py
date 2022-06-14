6043ab8a40a75d9d55dd7168d6696a0eb5a7fe09
G-Goncalves <goncalo.mota.goncalves@gmail.com>
2022-06-14 18:51:35 +0000

    Added randaug import to innit

--- AST/modules/__init__.py
+++ AST/modules/__init__.py
@@ -2,3 +2,4 @@ from torch.utils.data import DataLoader as My_DataLoader
 from AST.modules.train_loop import train
 from AST.modules.classifier import ASTModel as My_Classifier
 from AST.modules.dataset import Spectrograms
+from AST.modules.augmentation import RandAug
